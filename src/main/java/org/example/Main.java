487d0b2f2d331f4e4ff7b42e58ca0e041068ad66
SaikiranBantaram <43469173+SaikiranBantaram@users.noreply.github.com>
2024-07-06 14:38:27 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("Output from java program");
         System.out.println(new Date());
         System.out.println("Hello");
+        System.out.println("Java8");
     }
 }
