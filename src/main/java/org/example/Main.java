c313eef92a6fb4142ff4360e5c2a63223bdc26f9
SaikiranBantaram <43469173+SaikiranBantaram@users.noreply.github.com>
2024-07-06 14:17:01 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Java");
+        System.out.println("Output from java program");
+        System.out.println(new Date());
+    }
+}
